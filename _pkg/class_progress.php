59278217549e4161f210c1cd2fde528dc89ad182
exa18 <julian.cenkier@wp.eu>
2023-08-02 03:25:23 +0200

    gms: assets

--- _pkg/class_progress.php
+++ _pkg/class_progress.php
@@ -8,7 +8,7 @@ class progressBar {
 		var  $delay;
 		var  $objSufix;
 	
-	function __construct($total,$status='',$objSufix = '_status',$delay=300,$objID='progress')
+	function __construct($total,$status='',$objSufix = '_status',$objID='progress',$delay=300)
 	{
 		$this->objID = $objID;
 		$this->objSufix = $objSufix;
@@ -16,44 +16,47 @@ class progressBar {
 		$this->status = $status;
 		$this->delay = $delay;
 	}
+	public function show(){
+		$m = $this->buildShow($this->objID);
+		$ms = $this->buildShow($this->objID.$this->objSufix, $this->buildStatus(0));
+		echo '<script language="javascript">'.$m.($this->status ? $ms : '').'</script>';
+		flush();
+	}
+	public function hide(){
+		$m = $this->buildHide($this->objID);
+		$ms = $this->buildHide($this->objID.$this->objSufix);
+		echo '<script language="javascript">'.$m.$ms.'</script>';
+		flush();
+	}
+    public function update($i){
+		$percent = intval($i/$this->total * 100);
+		$st = 'document.getElementById("'.$this->objID.$this->objSufix.'").innerHTML="<div>'.$this->buildStatus($i).'</div>";';
+		echo '<script language="javascript">
+		document.getElementById("'.$this->objID.'").innerHTML="<div style=\"width:'.$percent.'%;\">&nbsp;</div>";'.($this->status ? $st : '').'
+		</script>'; 
+		flush();
+	}
 	
-	function showBuild($s,$a=''){
+	private function buildShow($s,$a=''){
 		if ($s) {
 			return $s.'=document.getElementById("'.$s.'");'.$s.'.innerHTML="<div>'.($a ? $a : '&nbsp;').'</div>";'.$s.'.style.display="block";'.$s.'.style.opacity=1;';
 		}
 	}
-	function show(){
-		$m = $this->showBuild($this->objID);
-		$ms = $this->showBuild($this->objID.$this->objSufix, $this->statusBuild(0));
-		echo '<script language="javascript">'.$m.($this->status ? $ms : '').'</script>';
-		flush();
-	}
-	function hideBuild($s){
+
+	private function buildHide($s){
 		if ($s) {
 			return $s.'=document.getElementById("'.$s.'");'.$s.'.style.opacity=0;'.$s.'.style.display="none";';
 		}
 	}
-	function hide(){
-		$m = $this->hideBuild($this->objID);
-		$ms = $this->hideBuild($this->objID.$this->objSufix);
-		echo '<script language="javascript">'.$m.$ms.'</script>';
-		flush();
-	}
-	function statusBuild($i){
+
+	private function buildStatus($i){
 		$s = $this->status;
 		if($s){
 			$p = $i . '/' . $this->total;
 			return str_replace("%s",$p,$s);
 		}
 	}
-    function update($i){
-		$percent = intval($i/$this->total * 100);
-		$st = 'document.getElementById("'.$this->objID.$this->objSufix.'").innerHTML="<div>'.$this->statusBuild($i).'</div>";';
-		echo '<script language="javascript">
-		document.getElementById("'.$this->objID.'").innerHTML="<div style=\"width:'.$percent.'%;\">&nbsp;</div>";'.($this->status ? $st : '').'
-		</script>'; 
-		flush();
-		}
+
 }
 
 ?>
--- _pkg/functions.php
+++ _pkg/functions.php
@@ -81,4 +81,15 @@ function tableGet($htmlContent){
 	return $table;
 }
 
+function imageSave($inPath,$outPath)
+{ //Download images from remote server
+    $in=    fopen($inPath, "rb");
+    $out=   fopen($outPath, "wb");
+    while ($chunk = fread($in,8192))
+    {
+        fwrite($out, $chunk, 8192);
+    }
+    fclose($in);
+    fclose($out);
+}
 ?>
